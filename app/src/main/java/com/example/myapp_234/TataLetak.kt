9ddc999f7d78c4d20d58d31daacc3a130f469ec1
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-10-16 15:16:47 +0700

    membuat function TataLetakBoxColumnRow

--- app/src/main/java/com/example/myapp_234/TataLetak.kt
+++ app/src/main/java/com/example/myapp_234/TataLetak.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -84,4 +85,9 @@ fun TataLetakRowColumn(modifier: Modifier) {
             Text(text = "Komponen3Kolom2")
         }
     }
+}
+
+@Composable
+fun TataLetakBoxColumnRow(modifier: Modifier) {
+
 }
