9f1a8b34b81370bcfb96573785616a51572a7461
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-10-16 14:40:36 +0700

    menambahkan modifier(pengubah compose) pada layout Column pada fungsi TataletakColumn

--- app/src/main/java/com/example/myapp_234/TataLetak.kt
+++ app/src/main/java/com/example/myapp_234/TataLetak.kt
@@ -1,13 +1,15 @@
 package com.example.myapp_234
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun TataLetakColumn(modifier: Modifier) {
-    Column {
+    Column(modifier = modifier.padding(top = 20.dp, start = 20.dp, end = 20.dp)) {
         Text(text = "Komponen1")
         Text(text = "Komponen2")
         Text(text = "Komponen3")
