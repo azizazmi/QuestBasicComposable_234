938f87a3e742a135c604046a92e80db46890e8d8
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-10-16 14:52:10 +0700

    menambahkan komponen untuk layout Box pada function TataLetakBox

--- app/src/main/java/com/example/myapp_234/TataLetak.kt
+++ app/src/main/java/com/example/myapp_234/TataLetak.kt
@@ -41,5 +41,11 @@ fun TataLetakBox(modifier: Modifier) {
         modifier = modifier
             .fillMaxHeight()
             .fillMaxWidth(), contentAlignment = Alignment.Center
-    ) {}
+    ) {
+        Text(text = "Box 1")
+        Text(text = "Column 1")
+        Text(text = "Row 1")
+        Text(text = "Box 2")
+        Text(text = "Column 2")
+    }
 }
