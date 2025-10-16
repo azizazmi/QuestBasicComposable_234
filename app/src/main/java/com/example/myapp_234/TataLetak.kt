1b4884ff668f0b025f2c454c6ff98cdfae46c629
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-10-16 14:57:41 +0700

    membuat komponen kedua dalam Column yaitu berupa layout row 2

--- app/src/main/java/com/example/myapp_234/TataLetak.kt
+++ app/src/main/java/com/example/myapp_234/TataLetak.kt
@@ -59,5 +59,11 @@ fun TataLetakColumnRow(modifier: Modifier) {
             Text(text = "Komponen2Baris1")
             Text(text = "Komponen3Baris1")
         }
+        //Baris2
+        Row {
+            Text(text = "Komponen1Baris2")
+            Text(text = "Komponen2Baris2")
+            Text(text = "Komponen3Baris2")
+        }
     }
 }
