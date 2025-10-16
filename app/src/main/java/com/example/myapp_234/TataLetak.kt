ddd1868d1f8dde002b5046f8d2715b7cdf858c92
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-10-16 15:44:25 +0700

    menambahkan spasi pada code variabel gambar

--- app/src/main/java/com/example/myapp_234/TataLetak.kt
+++ app/src/main/java/com/example/myapp_234/TataLetak.kt
@@ -100,7 +100,7 @@ fun TataLetakRowColumn(modifier: Modifier) {
 
 @Composable
 fun TataLetakBoxColumnRow(modifier: Modifier) {
-    val gambar =painterResource(id = R.drawable.baguette)
+    val gambar = painterResource(id = R.drawable.baguette)
     Column {
         Box(
             modifier = modifier
